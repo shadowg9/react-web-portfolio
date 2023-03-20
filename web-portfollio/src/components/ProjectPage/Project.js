653591aa5de5d5530e037aabda999484b2001355
Mohammed Sajjad <mohammedsajjad1017@gmail.com>
2023-03-20 02:04:10 -0500

    Button Reveal

--- web-portfollio/src/components/ProjectPage/Project.js
+++ web-portfollio/src/components/ProjectPage/Project.js
@@ -7,8 +7,37 @@ import './Project.css'
 function pics() {
 
     const handleClick = () => {
-        var x = document.getElementsByClassName('hiddenText');
-        x.style.display = "block";
+        var x = document.getElementById('hiddenText');
+        var x2 = document.getElementById('hiddenText2');
+        var x3 = document.getElementById('hiddenText3');
+        var x4 = document.getElementById('hiddenText4');
+        var x5 = document.getElementById('hiddenText5');
+        var x6 = document.getElementById('hiddenText6');
+        var x7 = document.getElementById('hiddenText7');
+        var x8 = document.getElementById('hiddenText8');
+
+        if (x.style.display === "none") {
+            x.style.display = "block";
+            x2.style.display = "block";
+            x3.style.display = "block";
+            x4.style.display = "block";
+            x5.style.display = "block";
+            x6.style.display = "block";
+            x7.style.display = "block";
+            x8.style.display = "block";
+        } else {
+            x.style.display = "none";
+            x2.style.display = "none";
+            x3.style.display = "none";
+            x4.style.display = "none";
+            x5.style.display = "none";
+            x6.style.display = "none";
+            x7.style.display = "none";
+            x8.style.display = "none";
+        }
+
+
+      
         
     }
     return (
@@ -84,15 +113,15 @@ function pics() {
 
        
         <button onClick={handleClick}>Information</button>
-        <h4 class="hiddenText">Purpose of Application</h4>
-        <p class="hiddenText">During my time in the training program offered by Cognixia USA, we had a Capstone project where the objective was to think up of a practical solution that can be of benefit to people and to demo a prototype of that app. 
+        <h4 id="hiddenText">Purpose of Application</h4>
+        <p id="hiddenText2">During my time in the training program offered by Cognixia USA, we had a Capstone project where the objective was to think up of a practical solution that can be of benefit to people and to demo a prototype of that app. 
         The purpose of this application was to have an online market where people are granted the freedom and accessibility to make fair exchanges as they see fit. And these types of exchanges are meant to bypass the high car dealership fees and contactual obligations.   </p>
-        <h4 class="hiddenText">Benefits</h4>
-        <p class="hiddenText">&#x2022; Allow users to more easily manage their cost savings by allowing them to buy and sell used cars</p>
-        <p class="hiddenText">&#x2022; Beneficial as it would give them accessibility and freedom to connect with others and make transactional exchanges with theior used vehicles</p>
-        <p class="hiddenText">&#x2022; Serves to cirvumvent the high car dealership fees and gives car owners the freedom to make fluid negotiations amongst themselves</p>
-        <h4 class="hiddenText">Description</h4>
-        <p class="hiddenText">This application was done with a Team I worked alongside with. It was 4 people and divided between frontend and backend. The backend focused on SpringBoot with Eclipse and having the servers and database up on AWS. 
+        <h4 id="hiddenText3">Benefits</h4>
+        <p id="hiddenText4">&#x2022; Allow users to more easily manage their cost savings by allowing them to buy and sell used cars</p>
+        <p id="hiddenText5">&#x2022; Beneficial as it would give them accessibility and freedom to connect with others and make transactional exchanges with theior used vehicles</p>
+        <p id="hiddenText6">&#x2022; Serves to cirvumvent the high car dealership fees and gives car owners the freedom to make fluid negotiations amongst themselves</p>
+        <h4 id="hiddenText7">Description</h4>
+        <p id="hiddenText8">This application was done with a Team I worked alongside with. It was 4 people and divided between frontend and backend. The backend focused on SpringBoot with Eclipse and having the servers and database up on AWS. 
         The Frontend used Visual Studio and was tasked with desigining the webpage and fetching API's to have it be displayed on the webpage. 
         I created and populated the tables with MySQL and the data inputted was the vehicles available on the market and users that registered on the webpage. 
         The Backend used Java to handle all the business logic and used Springboot because it simplified the building process, allowed for dependency injections, and was used to connect to the database.
